bc261abd32d1707296da5d0933f6f97007f7de0d
gioannides <gi316@ic.ac.uk>
2018-02-27 19:41:05 +0000

    Fixed em' bugs

---
+++ c_translator/formative/hh.c
@@ -0,0 +1,26 @@
+int main()
+{
+	int y;
+	int x=5;
+	int s;
+	y=((x+s-x)/x)*x;
+	while(y<25) {
+
+		if( y < 15 ) {
+
+		y = y +1;
+		}
+		else if( y ==15) {
+
+			y = y+5;
+		}
+ 	
+		else{
+
+			y = y+5;
+			return y;
+
+		}
+	
+	}
+ }
