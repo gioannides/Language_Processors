e59d7935ea5458835912ba121e2f2a73e50a0189
gioannides <gi316@ic.ac.uk>
2018-03-09 17:08:31 +0000

    Added ternary operator functionlity. Seems to work

--- c_compiler/tests/do_while.c
+++ c_compiler/tests/do_while.c
@@ -3,9 +3,17 @@ int a = 1 + 2;
 int main() {
 	int x = a + 97;
 	do {
+		do{
+
+			do{
+				x+=1;
+			}while(0);
+
+		x-=2;
+		}while(x>50);
 	
 		x = x-1;
-	}while(x != 0);
+	}while(x >= 0);
 	
 	return x;
 
---
+++ c_compiler/tests/increment.c
@@ -0,0 +1,26 @@
+
+int a = 1 + 2;
+int main() {
+	int b =0;
+	int j = 0;
+	int k = 0;
+	int x = 100;
+	int i = 0;
+	for(i; i < 89; i=i+1) {
+		for(j;j<89; j=j+1){
+			for(k;k<90;k=k+1){
+
+				while( x > 0){
+					x=x-1;
+					b = b +1;
+				}
+			}
+		}
+	}
+	
+	
+	
+
+	return b;
+
+}
